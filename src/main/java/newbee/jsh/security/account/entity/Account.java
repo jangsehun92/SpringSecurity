a1a2f44c34872a6ad520fb3f37db06b07f37280c
jangsehun92 <jangsehun92@gmail.com>
2021-11-18 23:01:15 +0900

    [ add ] Account Entity, AccountRepository

--- src/main/java/newbee/jsh/security/account/entity/Account.java
+++ src/main/java/newbee/jsh/security/account/entity/Account.java
@@ -1,5 +1,71 @@
 package newbee.jsh.security.account.entity;
 
-public class Account {
-    
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Entity
+@Table
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter@ToString
+public class Account implements UserDetails{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String name;
+    //role
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
 }
